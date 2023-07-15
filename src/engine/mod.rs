e8f8f9ccaf01a14541a188fa1f4e4739eac48d4c
jarvarvarvis <jarvistrigo@gmail.com>
2023-07-16 00:35:43 +0200

    #8 Added a missing a

--- src/engine/mod.rs
+++ src/engine/mod.rs
@@ -1,4 +1,4 @@
-//! This is module that provides various utilities for the engine.
+//! This is a module that provides various utilities for the engine.
 //!
 //! Module overview:
 //! - frame_time: Provides frame count, FPS and delta time measurement functionality
